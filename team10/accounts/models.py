28e68335cea99de3875c2ee8436daed360ad296a
hyunju <hyunju0618@naver.com>
2023-12-27 10:46:12 +0900

    중복 로그인2

--- team10/accounts/models.py
+++ team10/accounts/models.py
@@ -16,17 +16,20 @@ class Profile(models.Model):
             self.nickname = self.user.username
         super().save(*args, **kwargs)
 
-class UserSession(models.Model):
+class UserSession(models.Model): #중복 로그인 방지 위해 사용자의 세션 정보 저장
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     session_key = models.CharField(max_length=40)
     created_at = models.DateTimeField(auto_now_add=True)
-    
-    #중복 로그인 방지
-    def block_duplicate_login(sender, request, user, **kwargs):
-        login_user_list = UserSession.objects.filter(user=user)
-        for user_session in login_user_list:
-            session = SessionStore(user_session.session_key)
-            session.delete()
-        session_key = request.session.session_key
-        UserSession.objects.create(user=user, session_key = session_key)
-    user_logged_in.connect(block_duplicate_login)
+
+#중복 로그인 방지
+def block_duplicate_login(sender, request, user, **kwargs): #사용자 로그인시 호출되는 함수
+    login_user_list = UserSession.objects.filter(user=user)
+    for user_session in login_user_list: #동일한 사용자에 대해 기존에 저장된 모든 세션 기록 찾아서 삭제
+        session = SessionStore(user_session.session_key)
+        # session.delete()
+        session['blocked'] = True
+        session.save()
+        
+    session_key = request.session.session_key #ㅐ로운 세션 기록 생성
+    UserSession.objects.create(user=user, session_key = session_key) #UserSession기록에 추가
+user_logged_in.connect(block_duplicate_login) #사용자가 로그인할 때마다 함수 호출되도록 user_logged_in 신호에 함수 연결
