66b089ad589145cb9ff3dfe99d78b4459768f912
hyunju <hyunju0618@naver.com>
2023-12-27 14:19:10 +0900

    로그인실패 메세지 출력 수정

--- team10/accounts/middleware.py
+++ team10/accounts/middleware.py
@@ -6,8 +6,6 @@ from django.shortcuts import redirect
 
 class BlockedMiddleware(MiddlewareMixin):
     def process_request(self, request):
-        print("block_duplicate_login called for user:", request.user)
-        
         blocked = request.session.pop('blocked', None) #세션에서 blocked키를 찾아 로그아웃 처리 진행
         if blocked:
             messages.info(request, '다른 기기에서 동일 아이디로 로그인되어 자동으로 로그아웃 되었습니다')
