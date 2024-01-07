617e68a5533b1a3185a8e8f730bc1ced65ad4277
hyunju <hyunju0618@naver.com>
2024-01-08 02:57:39 +0900

    서비스 알림창 코드 완료

--- team10/static/assets/js/capture.js
+++ team10/static/assets/js/capture.js
@@ -1,5 +1,67 @@
+// let postureStatusCounts = {};  // 자세 상태를 추적하는 객체
+// let notificationActive = false;  // 알림 활성화 상태
 
+// function updatePostureStatusCounts(class_name) {
+//   // 나쁜 자세 카운트 증가
+//   if (class_name !== 0 && class_name !== -1) {
+//       postureStatusCounts[class_name] = (postureStatusCounts[class_name] || 0) + 1;
+//   } else {
+//       // 다른 결과(좋은 자세 또는 감지 불가)가 나오면 카운트 리셋
+//       postureStatusCounts = {};
+//   }
 
+//   // 연속된 나쁜 자세 감지
+//   if (postureStatusCounts[class_name] === 20 && !notificationActive) {
+//       makeNoti();  // 알림 생성
+//       notificationActive = true;
+//   }
+
+//   // 연속된 좋은 자세 감지
+//   if (class_name === 0 && notificationActive) {
+//       if (postureStatusCounts[class_name] === 10) {
+//           closeNotification();  // 알림 종료
+//           notificationActive = false;
+//       }
+//   }
+// }
+
+//----------------------알림 설정---------------------------------
+
+let badPostureCount = 0; // 나쁜 자세 카운트
+let goodPostureCount = 0; // 좋은 자세 카운트
+let notificationActive = false;  // 알림 활성화 상태
+
+function updatePostureStatusCounts(class_name) {
+  // 나쁜 자세 카운트 증가 (0과 -1을 제외한 모든 경우)
+  if (class_name !== 0 && class_name !== -1) {
+      badPostureCount++;
+      goodPostureCount = 0; // 나쁜 자세가 감지되면 좋은 자세 카운트 리셋
+  } else if (class_name === 0) {
+      // 좋은 자세가 감지되면 좋은 자세 카운트 증가, 나쁜 자세 카운트 리셋
+      goodPostureCount++;
+      badPostureCount = 0;
+  } else {
+      // 감지 불가 상태가 나오면 모든 카운트 리셋
+      badPostureCount = 0;
+      goodPostureCount = 0;
+  }
+
+  // 연속된 나쁜 자세 감지 (1분 동안 누적) => 20으로 바꾸기
+  if (badPostureCount === 2 && !notificationActive) {
+      makeNoti();  // 알림 생성
+      notificationActive = true;
+  }
+
+  // 연속된 좋은 자세 감지 => 10으로 바꾸기
+  if (goodPostureCount === 2 && notificationActive) {
+      closeNotification();  // 알림 종료
+      notificationActive = false;
+  }
+}
+
+//-------------------------------------------------------------------
+    
+    
     // The width and height of the captured photo. We will set the
     // width to the value defined here, but the height will be
     // calculated based on the aspect ratio of the input stream.
@@ -136,17 +198,21 @@
 
     function isBadPosture(num){
       let videoElement = document.getElementById('video');
+      updatePostureStatusCounts(num);
       if (num==0) {
+        console.log("badPostureCount: ", badPostureCount, ", goodPostureCount: ", goodPostureCount)
         videoElement.style.border = '8px solid lime';
         document.getElementById('posture-status').style.color = 'lime'; 
         return 'Good Posture';
       } 
       if(num==-1){
+        console.log("badPostureCount: ", badPostureCount, ", goodPostureCount: ", goodPostureCount)
         videoElement.style.border = ''
         document.getElementById('posture-status').style.color = 'blue';
         return 'Unable to detect posture';
       }
       else {
+        console.log("badPostureCount: ", badPostureCount, ", goodPostureCount: ", goodPostureCount)
         videoElement.style.border = '8px solid red';
         document.getElementById('posture-status').style.color = 'red';
         return 'Bad Posture';
@@ -315,3 +381,4 @@
           console.error('getUserMedia() error:', e);
         });
   */
+
