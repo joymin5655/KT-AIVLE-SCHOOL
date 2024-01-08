8fb7f576a58c4df0076dbfda7c6e85555cddf77e
sbchae11 <sbchae11@gmail.com>
2024-01-08 14:51:56 +0900

    좌우반전 적용

--- team10/static/assets/js/capture.js
+++ team10/static/assets/js/capture.js
@@ -245,6 +245,8 @@ function updatePostureStatusCounts(class_name) {
       canvas.height = height;
       // console.log(width);
       // console.log(height);
+      context.translate(video.width, 0);
+      context.scale(-1, 1);
       context.drawImage(video, 0, 0, width, height);
 
       var picdata = canvas.toDataURL('image/png');
