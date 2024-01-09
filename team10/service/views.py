c89dc68de4890083e13ac604ff10ac38001ad090
donee <dlehsrb1588@ajou.ac.kr>
2024-01-10 01:04:35 +0900

    mlflow 방식으로 model 불러오는 방식 변경

--- team10/service/views.py
+++ team10/service/views.py
@@ -15,15 +15,31 @@ import pandas as pd
 from .preprocessing import calculate_angle, calculate_distance, selected_landmarks, landmark_description, stretching_selected_landmarks
 import os
 import time
+import mlflow
 
 from django.contrib.auth.decorators import login_required
 
+# run ID와 모델 이름 설정
+run_id_pose = "7c0f50aeb0424ca7a2b8aaba54a8993e"
+model_name_pose = "model_pose_final"
 
-model_path = os.path.join(os.getcwd(), 'service\pose_classification_model.pkl')
-pose_model = joblib.load(model_path) # 여기 삭제하고 특정 이벤트 발생시 모델을 로드하도록.
+# 포즈 모델을 불러오는 코드
+model_uri_pose = f"runs:/{run_id_pose}/{model_name_pose}"
+pose_model = mlflow.sklearn.load_model(model_uri_pose)
 
-stretching_model_path = os.path.join(os.getcwd(), 'service\pose_classification_model_stretch_final.pkl')
-stretching_model = joblib.load(stretching_model_path) # 여기 삭제하고 특정 이벤트 발생시 모델을 로드하도록.
+# run ID와 모델 이름 설정
+run_id_stretch = "7c0f50aeb0424ca7a2b8aaba54a8993e"
+model_name_stretch = "model_stretch_final"
+
+# 스트레칭 모델을 불러오는 코드
+model_uri_stretch = f"runs:/{run_id_stretch}/{model_name_stretch}"
+stretching_model = mlflow.sklearn.load_model(model_uri_stretch)
+    
+# model_path = os.path.join(os.getcwd(), 'service\pose_classification_model.pkl')
+# pose_model = joblib.load(model_path) # 여기 삭제하고 특정 이벤트 발생시 모델을 로드하도록.
+
+# stretching_model_path = os.path.join(os.getcwd(), 'service\pose_classification_model_stretch_final.pkl')
+# stretching_model = joblib.load(stretching_model_path) # 여기 삭제하고 특정 이벤트 발생시 모델을 로드하도록.
 
 
 #임시로 만들었습니다
