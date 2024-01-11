6959a26b16cdb6f27c6a8ef6b6453e7b9d3e4a02
hyunju <hyunju0618@naver.com>
2024-01-11 11:08:00 +0900

    도와주세요

--- team10/service/views.py
+++ team10/service/views.py
@@ -20,7 +20,7 @@ import mlflow
 from django.contrib.auth.decorators import login_required
 
 # MLflow Tracking URI 설정
-mlflow.set_tracking_uri("file:///C:/Users/user/big project/Test_final/team10/team10/service/mlruns")
+mlflow.set_tracking_uri("C:\\Users\\user\\TEAM10\\team10\\team10\\service\\mlruns")
 
 # 포즈 run ID와 모델 이름 설정
 run_id_pose = "6b8c4018707341eca7bf4e57438cc697"
